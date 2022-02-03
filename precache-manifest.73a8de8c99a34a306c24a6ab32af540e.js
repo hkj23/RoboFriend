8399fd459556f4d3687199a35cd13e492cfc4be3
unknown <harshkj2k@gmail.com>
2022-02-03 17:36:17 +0530

    Updates

---
+++ precache-manifest.73a8de8c99a34a306c24a6ab32af540e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd223799656746ee375edbe74d1fd963",
+    "url": "/RoboFriend/index.html"
+  },
+  {
+    "revision": "9a9be5e1314653adf8c6",
+    "url": "/RoboFriend/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "e792cf2f727b51c0a109",
+    "url": "/RoboFriend/static/css/main.ea3575ba.chunk.css"
+  },
+  {
+    "revision": "9a9be5e1314653adf8c6",
+    "url": "/RoboFriend/static/js/2.ab51fc7f.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/RoboFriend/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "e792cf2f727b51c0a109",
+    "url": "/RoboFriend/static/js/main.858edb3a.chunk.js"
+  },
+  {
+    "revision": "a49298144ba8d28263ca",
+    "url": "/RoboFriend/static/js/runtime~main.cd267bc5.js"
+  }
+]);
